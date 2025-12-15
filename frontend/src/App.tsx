865ae18c6d46e0fe514e9411b88b8831e8b0aca4
biswa18121997 <biswajitshrm6@gmail.com>
2025-12-15 14:43:48 +0530

    Implement user redirection on app load
    Add navigation logic to redirect users based on authentication status.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet , useNavigate} from "react-router-dom";
 import Layout from "./components/Layout";
 import { UserProvider } from "./state/UserContext";
+import { useEffect } from "react";
 
 const App = () => {
+  const navigate = useNavigate();
+  const location = window.location;
+  useEffect(() => {
+    if(location.pathname == '/') {
+
+      if(!localStorage.getItem('userAuth'))
+        navigate('/login');
+      else 
+        navigate('/dashboard');
+    }
+  }, []);
   return (
     <UserProvider>
       <Layout>
